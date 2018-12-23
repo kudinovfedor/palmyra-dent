6fb91782b2413fffc03d8d4ec43267173b42a46f
Fedor Kudinov <fedor_kudinov@adoriasoft.com>
2018-12-23 14:41:10 +0200

    Add header, footer html+css;

--- footer.php
+++ footer.php
@@ -11,12 +11,94 @@
         </div><!-- .pre-footer end-->
     <?php endif; ?>
 
-    <div class="copyright">
-        <p class="container">
-            <?php _e('Developed by', 'brainworks') ?>
-            <a href="https://brainworks.com.ua/" target="_blank">BRAIN WORKS</a>
-            &copy; <?php echo date('Y'); ?>
-        </p>
+    <div class="footer-main">
+        <div class="container">
+            <div class="footer-before d-flex flex-wrap justify-content-between align-items-center">
+                <div class="footer-item footer-logo">
+                    <div class="logo"><?php get_default_logo_link('logo-f.png'); ?></div>
+                </div>
+                <?php if (has_nav_menu('main-nav')) { ?>
+                    <nav class="footer-item footer-nav nav js-menu">
+                        <button type="button" tabindex="0" class="menu-item-close menu-close js-menu-close"></button>
+                        <?php wp_nav_menu(array(
+                            'theme_location' => 'main-nav',
+                            'container' => false,
+                            'menu_class' => 'menu-container',
+                            'menu_id' => '',
+                            'fallback_cb' => 'wp_page_menu',
+                            'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                            'depth' => 3
+                        )); ?>
+                    </nav>
+                <?php } ?>
+                <div class="footer-item footer-btn">
+                    <button type="button" class="button-medium button-inverse <?php the_lang_class('js-appointment'); ?>">
+                        <?php _e('Record to reception', 'brainworks'); ?>
+                    </button>
+                </div>
+            </div>
+            <div class="footer-after d-flex flex-wrap justify-content-between">
+                <?php
+                $address = get_theme_mod('bw_additional_address');
+                $email = get_theme_mod('bw_additional_email');
+                if (!empty($address)) { ?>
+                    <div class="footer-item">
+                        <i class="fas fa-fw fa-home" aria-hidden="true"></i> <?php echo esc_html($address); ?>
+                    </div>
+                <?php }
+                if (!empty($email)) { ?>
+                    <div class="footer-item">
+                        <i class="fas fa-fw fa-envelope" aria-hidden="true"></i>
+                        <a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
+                    </div>
+                <?php } ?>
+                <?php if (has_phones()) { ?>
+                    <div class="footer-item footer-phone">
+                        <ul class="phone">
+                            <?php foreach (get_phones() as $phone) {
+                                $tel = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+                                ?>
+                                <li class="phone-item">
+                                    <a href="tel:<?php echo esc_attr(get_phone_number($tel)); ?>"
+                                       class="phone-number">
+                                        <?php echo str_replace('+38', '', $phone); ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                <?php } ?>
+                <?php if (has_social()) { ?>
+                    <div class="footer-item footer-social">
+                        <ul class="social">
+                            <?php foreach (get_social() as $social) { ?>
+                                <li class="social-item">
+                                    <a href="<?php echo esc_attr(esc_url($social['url'])); ?>" class="social-link" target="_blank">
+                                        <i class="<?php echo esc_attr($social['icon']); ?>" aria-hidden="true"
+                                           aria-label="<?php echo esc_attr($social['text']); ?>"></i>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="footer-item col-sm-6">
+                    &copy; <?php bloginfo('name') ?> - <?php _e('All rights reserved', 'brainworks') ?>
+                </div>
+                <div class="footer-item col-sm-6 text-right">
+                    <?php _e('Developed by', 'brainworks') ?>
+                    <a href="https://brainworks.com.ua" target="_blank">Brain Works</a>
+                    &copy; <?php echo date('Y'); ?>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
 
--- header.php
+++ header.php
@@ -17,61 +17,94 @@
 <?php wp_body(); ?>
 
 <div class="wrapper">
-    <?php /*
-    <div class="pre-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    <nav class="second-menu">
+
+    <header class="header">
+        <div class="header-before">
+            <div class="container d-flex flex-wrap">
+                <?php
+                $address = get_theme_mod('bw_additional_address');
+                $timeWork = get_theme_mod('bw_additional_work_schedule');
+                if (!empty($address)) { ?>
+                    <div class="header-item">
+                        <i class="fas fa-fw fa-home" aria-hidden="true"></i> <?php echo esc_html($address); ?>
+                    </div>
+                <?php }
+                if (!empty($timeWork)) { ?>
+                    <div class="header-item">
+                        <i class="far fa-fw fa-clock" aria-hidden="true"></i> <?php echo esc_html($timeWork); ?>
+                    </div>
+                <?php } ?>
+                <?php if (has_phones()) { ?>
+                    <div class="header-item header-phone">
+                        <ul class="phone">
+                            <?php foreach (get_phones() as $phone) {
+                                $tel = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
+                                ?>
+                                <li class="phone-item">
+                                    <a href="tel:<?php echo esc_attr(get_phone_number($tel)); ?>"
+                                       class="phone-number">
+                                        <?php echo str_replace('+38', '', $phone); ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                <?php } ?>
+                <?php if (has_social()) { ?>
+                    <div class="header-item header-social">
+                        <ul class="social">
+                            <?php foreach (get_social() as $social) { ?>
+                                <li class="social-item">
+                                    <a href="<?php echo esc_attr(esc_url($social['url'])); ?>" class="social-link" target="_blank">
+                                        <i class="<?php echo esc_attr($social['icon']); ?>" aria-hidden="true"
+                                           aria-label="<?php echo esc_attr($social['text']); ?>"></i>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                <?php } ?>
+                <?php if (function_exists('pll_the_languages')) { ?>
+                    <div class="header-item header-lang">
+                        <ul class="lang">
+                            <?php pll_the_languages(array(
+                                'show_flags' => 0,
+                                'show_names' => 1,
+                                'hide_if_empty' => 0,
+                                'display_names_as' => 'slug'
+                            )); ?>
+                        </ul>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="header-main">
+            <div class="container d-flex flex-wrap justify-content-between align-items-center">
+                <div class="header-item header-logo">
+                    <div class="logo"><?php get_default_logo_link(); ?></div>
+                </div>
+                <?php if (has_nav_menu('main-nav')) { ?>
+                    <nav class="header-item header-nav nav js-menu">
+                        <button type="button" tabindex="0" class="menu-item-close menu-close js-menu-close"></button>
                         <?php wp_nav_menu(array(
-                            'theme_location' => 'second-menu',
+                            'theme_location' => 'main-nav',
                             'container' => false,
                             'menu_class' => 'menu-container',
                             'menu_id' => '',
                             'fallback_cb' => 'wp_page_menu',
                             'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                            'depth' => 2
+                            'depth' => 3
                         )); ?>
                     </nav>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    Some info here
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                    Some info here
-                </div>
-            </div>
-        </div>
-    </div>
-    */ ?>
-
-    <header class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
-
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                    <?php get_search_form(); ?>
+                <?php } ?>
+                <div class="header-item header-btn">
+                    <button type="button" class="button-medium <?php the_lang_class('js-appointment'); ?>">
+                        <?php _e('Record to reception', 'brainworks'); ?>
+                    </button>
                 </div>
             </div>
         </div>
     </header>
-
-    <?php if (has_nav_menu('main-nav')) { ?>
-        <nav class="nav js-menu">
-            <button type="button" tabindex="0" class="menu-item-close menu-close js-menu-close"></button>
-            <?php wp_nav_menu(array(
-                'theme_location' => 'main-nav',
-                'container' => false,
-                'menu_class' => 'menu-container',
-                'menu_id' => '',
-                'fallback_cb' => 'wp_page_menu',
-                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'depth' => 3
-            )); ?>
-        </nav>
-    <?php } ?>
 
     <div class="container js-container">
 
